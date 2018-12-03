6abfc1e150e4c59f7a8ab26a26511e9127a75f6b
Doris Liu <doris@DorisdeMacBook-Pro-3.local>
2018-12-03 10:50:38 -0800

    Update server.R

--- crime_in_seattle/server.R
+++ crime_in_seattle/server.R
@@ -1,26 +1,30 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
-
-# Define server logic required to draw a histogram
+data <- read.csv(file = "data/updated_crime_data.csv")
 shinyServer(function(input, output) {
-   
-  d <- reactive({
-    data <- crime_data %>% filter()
-    return(data)
+  output$plot <- renderPlot({
+    if(is.null(input$allneighborhood)| is.null(input$allsubcat))
+      return()
+    selected_data <- filter(data, data$Year >= input$years[1], data$Year <= input$years[2],
+                            data$Neighborhood %in% toupper(input$allneighborhood),
+                            data$Crime.Subcategory %in% toupper(input$allsubcat)
+    )
+    data_use <- count(group_by(selected_data, Year, Neighborhood))
+    
+    ggplot(data_use, aes(Year, n, group = data_use$Neighborhood)) +
+      geom_line(aes(col = Neighborhood)) +
+      geom_point()
   })
   
-  output$graphone <- renderPlot({
-    
+  output$select_nh <-renderUI({
+    checkboxGroupInput("allneighborhood", label = h3("Select neighborhood"),
+                       tolower(unique(data$Neighborhood)))
+  })
+  
+  output$select_type <- renderUI ({
+    checkboxGroupInput("allsubcat", label = h3("Select categories of crime"),
+                       tolower(unique(data$Crime.Subcategory)), tolower(unique(data$Crime.Subcategory)))
   })
   
 })
